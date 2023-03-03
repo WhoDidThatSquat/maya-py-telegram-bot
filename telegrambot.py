eba6eb35a171adde607a78464848694ee0ac521a
George Dragu <49279314+george-dragu@users.noreply.github.com>
2023-03-03 12:24:25 +0200

    Update telegrambot.py

--- telegrambot.py
+++ telegrambot.py
@@ -22,8 +22,8 @@ def covid_city_stats(update, context):
     try:
         aux = update.message.text
         city_stats_cmd = aux.split()
-        city = city_stats_cmd[1]
-        city_stats = gccs.get_covid_city_stats(city)
+        user_input = city_stats_cmd[1]
+        city_stats = gccs.get_covid_city_stats(user_input)
 
         for i in city_stats:
                 judet = 'Judet: {} \n'.format(i['Judet'])
@@ -42,7 +42,7 @@ def covid_city_stats(update, context):
             update.message.from_user.id,
             update.message.from_user.username,
             update.message.from_user.first_name,
-            update.message.from_user.last_name, city
+            update.message.from_user.last_name, user_input
             )
 
 def openai_jokes(update, context):
@@ -50,15 +50,24 @@ def openai_jokes(update, context):
     #if(reply == True):
     try:
         aux = update.message.text
-        text = aux.split(' ',1)[1]
-        completion = call_openai.req_openai(text)
+        user_input = aux.split(' ',1)[1]
+        completion = call_openai.req_openai(user_input)
         update.message.reply_text(completion)
     finally:
-        print("TODO:To add logging")
+        log_action(
+            update.message.chat.id,
+            update.message.chat.title,
+            update.message.chat.type,
+            update.message.from_user.id,
+            update.message.from_user.username,
+            update.message.from_user.first_name,
+            update.message.from_user.last_name,
+            user_input
+            )
 
-def log_action(chat_id, title, chat_type, user_id , username, first_name, last_name, city):
+def log_action(chat_id, title, chat_type, user_id , username, first_name, last_name, user_input):
     n0w = datetime.now()
-    log = {"chat_id": None, "chat_name": None, "chat_type": None, "user_id": None, "username": None, "first_name": None, 'last_name': None, "city": None, "datetime": None}
+    log = {"chat_id": None, "chat_name": None, "chat_type": None, "user_id": None, "username": None, "first_name": None, 'last_name': None, "user_input": None, "datetime": None}
     log['chat_id'] = chat_id
     log['chat_name'] = title
     log['chat_type'] = chat_type
@@ -66,10 +75,10 @@ def log_action(chat_id, title, chat_type, user_id , username, first_name, last_n
     log['username'] = username
     log['first_name'] = first_name
     log['last_name'] = last_name
-    log['city'] = city.upper()
+    log['user_input'] = user_input.upper()
     log['datetime'] = str(n0w)
     logs = json.dumps(log)
-    f = open("covid_city_stats.log","a")
+    f = open("user_actions.log","a")
     f.write(logs + "\n")
     f.close()
 
