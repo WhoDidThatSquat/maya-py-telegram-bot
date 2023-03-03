f0a6b35fa9ec56423e7a080eda3ee12a5d0fd956
George Dragu <49279314+george-dragu@users.noreply.github.com>
2023-03-03 12:25:23 +0200

    Update call_openai.py

--- call_openai.py
+++ call_openai.py
@@ -1,11 +1,12 @@
 import os
 import openai
+import api_key
 def req_openai(text_to_send):
-    openai.api_key = ""
+    openai.api_key = api_key.openai_api_key()
     completion = openai.Completion.create(
-      model="text-davinci-002",
+      model="text-davinci-003",
       prompt=text_to_send,
       max_tokens=200,
-      temperature=0.5
+      temperature=1.2
     )
     return(completion.choices[0].text)
